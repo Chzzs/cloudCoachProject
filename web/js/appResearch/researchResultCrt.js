9651ebb33ac235e032e924eb1838d3e81f6596d2
Vouill <vouillon.thibaud@gmail.com>
2016-01-09 15:21:23 +0100

    search front linked to end side

--- web/js/appResearch/researchResultCrt.js
+++ web/js/appResearch/researchResultCrt.js
@@ -4,25 +4,22 @@ filterFunction.$inject=['$scope','$http','$location'];
 
 //Définir le comportement du controller
 function filterFunction($scope,$http,$location){
-     
-   console.log($location.search().search); // get url param to use in http service
 
-    var training1={title:'training1',duration:'8 min'};
-    var training2={title:'training2',duration:'2 jours'};
-    var trainingListLocal=[training1,training2];
+  $http({
+   method: 'GET',
+   url: '/search?query='+$location.search().query
+   }).then(function successCallback(response) {
+console.log('received :');
+console.log(response.data);
+   $scope.trainingList=response.data.trainings;
+    $scope.exerciceList=response.data.exercices;
 
-    var exercice1={title:'exercice1',duration:' 3 heures'};
-    var exercice2={title:'exercice2',duration:'4 min'};
-    var exerciceListLocal=[exercice1,exercice2];
-
-    if(trainingListLocal.length!=0)
-    $scope.trainingBool=true;// set as true when data training
-    
-    if(exerciceListLocal.length!=0)
-    $scope.exerciceBool=true;// set as true when data exercice
-    
-    $scope.trainingList=trainingListLocal;
-    $scope.exerciceList=exerciceListLocal;
-
+   }, function errorCallback(response) {
+    // called asynchronously if an error occurs
+   // or server returns response with an error status.
+  });
 
 };
+
+
+
